3238ccc43f97edf17244cd70a5431a2ee65e012c
Balazs Kegl <balazs.kegl@gmail.com>
2015-02-08 23:14:20 +0100

    R(-1).AB(20;RF(100;5))

--- model.py
+++ model.py
@@ -1,5 +1,4 @@
-from sklearn.ensemble import AdaBoostClassifier 
-from sklearn.tree import DecisionTreeClassifier 
+from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
 from sklearn.preprocessing import Imputer
 import numpy as np
 
@@ -9,9 +8,9 @@ def model(X_train, y_train, X_test):
 	where_are_NaNs = np.isnan(X_test)
 	X_test[where_are_NaNs] = -1
 
-	clf = AdaBoostClassifier(base_estimator=DecisionTreeClassifier(max_depth=5, max_features=20),
-                         n_estimators=1000)
-	clf.fit(X_train, y_train)
+	clf = AdaBoostClassifier(base_estimator=RandomForestClassifier(max_depth=5, n_estimators=100),
+                         n_estimators=20)
+ 	clf.fit(X_train, y_train)
 	y_pred = clf.predict(X_test)
 	y_score = clf.predict_proba(X_test)
 	return y_pred, y_score
