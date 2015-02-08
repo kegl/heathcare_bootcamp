c5582c933dd0d577176409a86906a43c0157b05a
Balazs Kegl <balazs.kegl@gmail.com>
2015-02-08 23:15:00 +0100

    MF.AB(20;RF(100;5))

--- model.py
+++ model.py
@@ -1,15 +1,12 @@
 from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
 from sklearn.preprocessing import Imputer
+from sklearn.pipeline import Pipeline
 import numpy as np
 
 def model(X_train, y_train, X_test):
-	where_are_NaNs = np.isnan(X_train)
-	X_train[where_are_NaNs] = -1
-	where_are_NaNs = np.isnan(X_test)
-	X_test[where_are_NaNs] = -1
-
-	clf = AdaBoostClassifier(base_estimator=RandomForestClassifier(max_depth=5, n_estimators=100),
-                         n_estimators=20)
+	clf = Pipeline([('imputer', Imputer(strategy='most_frequent')),
+		('rf', AdaBoostClassifier(base_estimator=RandomForestClassifier(max_depth=5, n_estimators=100),
+                         n_estimators=20))])
  	clf.fit(X_train, y_train)
 	y_pred = clf.predict(X_test)
 	y_score = clf.predict_proba(X_test)
