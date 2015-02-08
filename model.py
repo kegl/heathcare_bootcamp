3ecf15309ec59b4d5b471ac0e6b5388c46f397e5
Balazs Kegl <balazs.kegl@gmail.com>
2015-02-08 23:13:32 +0100

    R(-1).AB(1000;5;20)

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
-from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import AdaBoostClassifier 
+from sklearn.tree import DecisionTreeClassifier 
 from sklearn.preprocessing import Imputer
 import numpy as np
 
@@ -8,7 +9,8 @@ def model(X_train, y_train, X_test):
 	where_are_NaNs = np.isnan(X_test)
 	X_test[where_are_NaNs] = -1
 
-	clf = GradientBoostingClassifier(n_estimators=1000, max_depth=5, max_features=20)
+	clf = AdaBoostClassifier(base_estimator=DecisionTreeClassifier(max_depth=5, max_features=20),
+                         n_estimators=1000)
 	clf.fit(X_train, y_train)
 	y_pred = clf.predict(X_test)
 	y_score = clf.predict_proba(X_test)
