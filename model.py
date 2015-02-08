7c24f5c0c14c7cd0c1f2290020b8e31260c970b5
Balazs Kegl <balazs.kegl@gmail.com>
2015-02-08 23:11:05 +0100

    R(-1).GB(100;3)

--- model.py
+++ model.py
@@ -1,10 +1,14 @@
-from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.preprocessing import Imputer
-from sklearn.pipeline import Pipeline
+import numpy as np
 
 def model(X_train, y_train, X_test):
-	clf = Pipeline([('imputer', Imputer(strategy='most_frequent')),
-		('rf', ExtraTreesClassifier(n_estimators=1000))])
+	where_are_NaNs = np.isnan(X_train)
+	X_train[where_are_NaNs] = -1
+	where_are_NaNs = np.isnan(X_test)
+	X_test[where_are_NaNs] = -1
+
+	clf = GradientBoostingClassifier(n_estimators=100, max_depth=3)
 	clf.fit(X_train, y_train)
 	y_pred = clf.predict(X_test)
 	y_score = clf.predict_proba(X_test)
