c47d5c60a6b20c91a1b60c7b4e5d5c541b41a741
Balazs Kegl <balazs.kegl@gmail.com>
2015-02-08 23:01:45 +0100

    RF(300)

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ from sklearn.preprocessing import Imputer
 from sklearn.pipeline import Pipeline
 
 def model(X_train, y_train, X_test):
-	clf = Pipeline([('imputer', Imputer()), 
+	clf = Pipeline([('imputer', Imputer(strategy='most_frequent')),
 		('rf', RandomForestClassifier(n_estimators=300))])
 	clf.fit(X_train, y_train)
 	y_pred = clf.predict(X_test)
