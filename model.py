b3660a4232ef1c5c7a1938e158be2b460af828f0
Balazs Kegl <balazs.kegl@gmail.com>
2015-02-22 17:25:02 +0100

    1

--- model.py
+++ model.py
@@ -4,9 +4,7 @@ from sklearn.pipeline import Pipeline
 import numpy as np
 
 def model(X_train, y_train, X_test):
-	clf = Pipeline([('imputer', Imputer(strategy='most_frequent')),
-		('rf', AdaBoostClassifier(base_estimator=RandomForestClassifier(max_depth=5, n_estimators=100),
-                         n_estimators=20))])
+	clf = Pipeline([('imputer', Imputer()), ('rf', RandomForestClassifier(n_estimators=50))])
  	clf.fit(X_train, y_train)
 	y_pred = clf.predict(X_test)
 	y_score = clf.predict_proba(X_test)
